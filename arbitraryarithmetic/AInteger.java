da83ad6839465feca01b75986b71f3121316bf54
prashantsinghaniya <prashantsinghaniya01@gmail.com>
2025-05-01 22:55:09 +0530

    add multiplication method

--- arbitraryarithmetic/AInteger.java
+++ arbitraryarithmetic/AInteger.java
@@ -135,4 +135,61 @@ public class AInteger{
         return new AInteger(leading ? "0" : sb.toString());
     }
 
+    public static AInteger multiplication(AInteger num1, AInteger num2) {
+        boolean num1_neg = false;
+        boolean num2_neg = false;
+        if(num1.value.charAt(0) == '-'){
+            num1.value = num1.value.substring(1);
+            if(num1.value.replaceFirst("^0+(?!$)","").equals("")){
+                return new AInteger("0");
+            }
+            num1_neg = true;
+        }
+        if(num2.value.charAt(0) == '-'){
+            num2.value = num2.value.substring(1);
+            if(num2.value.replaceFirst("^0+(?!$)","").equals("")){
+                return new AInteger("0");
+            }
+            num2_neg = true;
+        }
+        if(num1_neg == true && num2_neg == false) return new AInteger("-" + AInteger.multiplication(num1, num2).getInt());
+        else if(num1_neg == false && num2_neg == true) return new AInteger("-" + AInteger.multiplication(num1, num2).getInt());
+        int len1 = num1.value.length();
+        int len2 = num2.value.length();
+        int[] n1 = new int[len1];
+        int[] n2 = new int[len2];
+        
+        // Fill n1
+        for (int i = len1 - 1, j = 0; i >= 0; i--, j++) {
+            n1[j] = num1.value.charAt(i) - '0';  // reverse order for easy calc
+        }
+        // Fill n2
+        for (int i = len2 - 1, j = 0; i >= 0; i--, j++) {
+            n2[j] = num2.value.charAt(i) - '0';
+        }
+        
+        int[] result = new int[len1 + len2];
+    
+        // Multiply digits by element to element
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                result[i + j] += n1[i] * n2[j];
+                // Handle carry
+                result[i + j + 1] += result[i + j] / 10;
+                result[i + j] %= 10;
+            }
+        }
+        
+        // Build string from result and skipping leading zeros
+        StringBuilder sb = new StringBuilder();
+        boolean leading = true;
+        for (int i = result.length - 1; i >= 0; i--) {
+            if (result[i] == 0 && leading) continue;
+            leading = false;
+            sb.append(result[i]);
+        }
+        
+        return new AInteger(leading ? "0" : sb.toString());
+    }
+
 }
